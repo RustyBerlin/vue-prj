3904b279b205080486545456babe85890c553ee8
RustyBerlin <kingsizeberlin@gmx.de>
2019-03-04 16:54:39 +0100

    added vue carousel

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,25 @@
       <div class="module">
         <Headline headline='Welcome to My Website' />
         <section>
+          <carousel :perPageCustom="[[0, 1], [640, 2], [1199, 4]]" :autoplay="true">
+            <slide>
+              <img :src="hero.image" alt="Alt-Text">
+            </slide>
+            <slide>
+              <img :src="hero.image" alt="Alt-Text">
+            </slide>
+            <slide>
+              <img :src="hero.image" alt="Alt-Text">
+            </slide>
+            <slide>
+              <img :src="hero.image" alt="Alt-Text">
+            </slide>
+            <slide>
+              <img :src="hero.image" alt="Alt-Text">
+            </slide>
+          </carousel>
+        </section>
+        <section>
           <h2>H2 - Headline</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -29,10 +48,12 @@
 </template>
 
 <script>
+  import config from '../config.json'
   import Header from '~/components/Header.vue'
   import Headline from '~/components/Headline.vue'
   import Footer from '~/components/Footer.vue'
-  import config from '../config.json'
+  import Carousel from 'vue-carousel/src/Carousel.vue'
+  import Slide from 'vue-carousel/src/Slide.vue'
 
   const pageTitle = 'Welcome'
 
@@ -40,13 +61,20 @@
     components: {
       Header,
       Headline,
-      Footer
+      Footer,
+      Carousel,
+      Slide,
 
     },
 
     head() {
       return {
         title: `${pageTitle} / ${config.title}`
+      }
+    },
+    data() {
+      return {
+        hero: config.hero,
       }
     }
   }
